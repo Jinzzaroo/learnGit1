834be35d70bd678a54ca3c4afa63290d3287351f
jyk <realzero101@gmail.com>
2023-07-25 15:34:05 +0900

    add setter getter for age

--- jinyng.py
+++ jinyng.py
@@ -1,14 +1,19 @@
 class Jinyng:
 
     def __init__(age, company, school):
-        self.age = age
+        self.__age = age
         self.school = school
         self.company = company
 
     def print():
-        print(f"Hi, I'm jinyng.\n I'm {self.age}"+
+        print(f"Hi, I'm jinyng.\n I'm {self.__age}"+
               f"I graduated from {self.school}.\n"+
               f"I work for {self.company}")
+
+    def setAge(age):
+        self.__age = age
+    def getAge():
+        return self.__age
 
 if __name__ == "__main__":
 
