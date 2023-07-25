42322ef3a8a9d9f301f5e3e3490e5b3ae509dd29
jyk <realzero101@gmail.com>
2023-07-25 16:12:41 +0900

    add setter and getter for school

--- jinyng.py
+++ jinyng.py
@@ -2,18 +2,21 @@ class Jinyng:
 
     def __init__(age, company, school):
         self.__age = age
-        self.school = school
+        self.__school = school
         self.company = company
 
     def print():
         print(f"Hi, I'm jinyng.\n I'm {self.__age}"+
-              f"I graduated from {self.school}.\n"+
+              f"I graduated from {self.__school}.\n"+
               f"I work for {self.company}")
 
     def setAge(age):
         self.__age = age
     def getAge():
         return self.__age
+
+    def setSchool(school):
+        return self.__school
 
 if __name__ == "__main__":
 
